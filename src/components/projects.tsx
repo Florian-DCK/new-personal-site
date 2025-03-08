aee4520aac89a84e20b36bb5558ca7405e20c40a
Florian Donckers <fdonckersf@gmail.com>
2025-03-09 00:11:18 +0100

    Amélioration du composant Projects avec une mise en page révisée et une description mise à jour. Changement de la classe de style pour le ScrollBar.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -16,41 +16,42 @@ export default function Projects() {
 					{t('projectsDescription')}
 				</p>
 				<div>
-					<ScrollArea>
-						<Card className="dark p-3 w-162 h-fit glass border-stone-200/20 ">
-							<CardHeader className="px-2 flex-row justify-between">
-								<CardTitle className="font-main font-bold text-lg">
-									Adopte un Cube
-								</CardTitle>
-								<Link
-									className={`${buttonVariants({
-										variant: 'outline',
-									})} bg-transparent border border-stone-200/30 transition-all text-white font-main hover:text-black hover:bg-white`}
-									href={'https://www.adopteuncube.com/'} target='_blank'>
-									Visiter
-								</Link>
-							</CardHeader>
-							<CardContent className="ps-2 pe-0 flex space-x-2">
-								<p className="text-stone-200/70 font-main">
-									C'est un site vitrine pour une communauté de joueurs centrés
-									principalement sur Minecraft. <br /> <br />
-									J'ai entièrement designé et mis en place ce site.
-								</p>
-								<Image
-									src="/AUC_Mockup.png"
-									alt="Visual of Adopte un Cube Website"
-									width={300}
-									height={100}
-									className=""></Image>
-							</CardContent>
-							<div className="flex space-x-2">
-								<Badge className="font-main font-bold">Next.js</Badge>
-								<Badge className="font-main font-bold bg-[#38bdf8]">
-									Tailwind CSS
-								</Badge>
-							</div>
-						</Card>
-						<ScrollBar></ScrollBar>
+					<ScrollArea className="dark w-full flex-nowrap pb-3 transition-all">
+						<div className='flex space-x-10'>
+							<Card className="dark p-3 w-162 h-fit glass border-stone-200/20 ">
+								<CardHeader className="px-2 flex-row justify-between">
+									<CardTitle className="font-main font-bold text-lg">
+										Adopte un Cube
+									</CardTitle>
+									<Link
+										className={`${buttonVariants({
+											variant: 'outline',
+										})} bg-transparent border border-stone-200/30 transition-all text-white font-main hover:text-black hover:bg-white`}
+										href={'https://www.adopteuncube.com/'}
+										target="_blank">
+										Visiter
+									</Link>
+								</CardHeader>
+								<CardContent className="ps-2 pe-0 flex space-x-2">
+									<p className="text-stone-200/70 font-main">
+										Site vitrine conçu et développé pour une communauté de joueurs, principalement axée sur Minecraft. J'ai pris en charge l'intégralité du design et de la mise en œuvre technique.
+									</p>
+									<Image
+										src="/AUC_Mockup.png"
+										alt="Visual of Adopte un Cube Website"
+										width={300}
+										height={100}
+										className=""></Image>
+								</CardContent>
+								<div className="flex space-x-2">
+									<Badge className="font-main font-bold">Next.js</Badge>
+									<Badge className="font-main font-bold bg-[#38bdf8]">
+										Tailwind CSS
+									</Badge>
+								</div>
+							</Card>
+						</div>
+						<ScrollBar orientation="horizontal"></ScrollBar>
 					</ScrollArea>
 				</div>
 			</div>
